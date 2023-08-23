9d2e72ce72726735b7449dc3476242c82bb237bb
eftpmc <61393419+eftpmc@users.noreply.github.com>
2023-08-22 20:26:30 -0400

    first setup

--- edensgarden/edensgarden.cpp
+++ edensgarden/edensgarden.cpp
@@ -1,20 +1,126 @@
-// edensgarden.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include <SFML/Audio.hpp>
 #include <iostream>
+#include <deque>
+#include <algorithm>
+#include <random>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+class MusicPlayer {
+private:
+	sf::Music music;
+	std::deque<std::string> trackQueue;
+	int currentTrackIndex = 0;
+
+	void playCurrentTrack() {
+		if (currentTrackIndex >= 0 && static_cast<size_t>(currentTrackIndex) < trackQueue.size()) {
+			if (!music.openFromFile(trackQueue[currentTrackIndex])) {
+				std::cerr << "Failed to load music file: " << trackQueue[currentTrackIndex] << "\n";
+			}
+			else {
+				music.play();
+			}
+		}
+	}
+
+public:
+	void playPause() {
+		if (music.getStatus() == sf::Music::Playing) {
+			music.pause();
+		}
+		else if (!trackQueue.empty()) {
+			playCurrentTrack();
+		}
+	}
+
+	void setVolume(float volume) {
+		music.setVolume(volume);
+	}
+
+	void next() {
+		if (!trackQueue.empty()) {
+			currentTrackIndex++;
+			if (static_cast<size_t>(currentTrackIndex) >= trackQueue.size()) {
+				currentTrackIndex = 0;
+			}
+			playCurrentTrack();
+		}
+	}
+
+	void previous() {
+		if (!trackQueue.empty()) {
+			if (--currentTrackIndex < 0) {
+				currentTrackIndex = trackQueue.size() - 1;
+			}
+			playCurrentTrack();
+		}
+	}
+
+	void shuffle() {
+		std::random_device rd;
+		std::mt19937 g(rd());
+		std::shuffle(trackQueue.begin(), trackQueue.end(), g);
+		currentTrackIndex = 0;
+		playCurrentTrack();
+	}
+
+	void addTrackToQueue(const std::string& trackPath) {
+		trackQueue.push_back(trackPath);
+	}
+
+	void removeCurrentTrack() {
+		if (!trackQueue.empty()) {
+			trackQueue.erase(trackQueue.begin() + currentTrackIndex);
+			playCurrentTrack();
+		}
+	}
+};
+
+int main() {
+	MusicPlayer player;
+
+	char choice;
+	while (true) {
+		std::cout << "\nMenu:\n";
+		std::cout << "a: Play/Pause, b: Volume, c: Previous, d: Next, e: Shuffle, f: Add Track, g: Remove Track, h: Exit\n";
+		std::cin >> choice;
+		std::cin.ignore();  // To consume the newline character
+
+		switch (choice) {
+		case 'a':
+			player.playPause();
+			break;
+		case 'b':
+			float volume;
+			std::cout << "Enter volume (0 to 100): ";
+			std::cin >> volume;
+			player.setVolume(volume);
+			break;
+		case 'c':
+			player.previous();
+			break;
+		case 'd':
+			player.next();
+			break;
+		case 'e':
+			player.shuffle();
+			break;
+		case 'f':
+		{
+			std::string trackPath;
+			std::cout << "Enter path of the track to add to queue: ";
+			std::getline(std::cin, trackPath);
+			player.addTrackToQueue(trackPath);
+			break;
+		}
+		case 'g':
+			player.removeCurrentTrack();
+			break;
+		case 'h':
+			return 0;
+		default:
+			std::cout << "Invalid choice\n";
+			break;
+		}
+	}
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
